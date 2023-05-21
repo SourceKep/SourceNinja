705404bc51a6ed6eb0ad0c167add4630dd773421
SourceKep <128210845+SourceKep@users.noreply.github.com>
2023-05-21 14:07:13 -0400

    Update Mainscript.lua

--- Mainscript.lua
+++ Mainscript.lua
@@ -1,18 +1,14 @@
-repeat task.wait() until game:isloaded()
-rconsoleprint("Hi enter your key: ")
-local Key = "/run1"
-local Input;
-Input = rconsoleinput()
-
-if Input == Key then
- rconsoleprint("Tnx")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/SourceNinja/main/Source.lua", true))()
-  wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/SourceNinja/main/Loading.lua", true))()
-
-else
-  rconsoleprint("Nothing")
-  local plr = game.Players.LocalPlayer
-  plr:Kick()
+local function Checker()
+ if not game:isloaded() then
+   pcall(function() 
+   local NewMessage = Instance.new("Message")
+   NewMessage.Text = "Client is waiting to load.")
+   end)
+ else
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/SourceNinja/main/Loading.lua", true))()
+ wait(1)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SourceKep/SourceNinja/main/NewMainScript.lua", true))()
+  end
  end
 
+Checker()
